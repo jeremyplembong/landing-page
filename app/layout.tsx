533fa5441c6bec252032c8c97ecf145a9f13f249
jeremyplembong <jeremyplembong@gmail.com>
2025-11-26 13:32:11 +0800

    added main holders for all navbars

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Rajdhani } from "next/font/google";
 import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav"
 
 const font = Rajdhani({
   weight: ['300', '400', '500', '600', '700'],
@@ -20,8 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${font.className} antialiased`}
-      >
+        className={`${font.className} antialiased`}>
+          <ResponsiveNav />
         {children}
       </body>
     </html>
