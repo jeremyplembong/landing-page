5ee19472bc59e4fc6c71f8aaa0478f94cb1f7746
jeremyplembong <jeremyplembong@gmail.com>
2025-11-28 21:11:20 +0800

    started mobilenav, finished overlay and navlinks

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,8 +1,26 @@
+import { NavLinks } from '@/constant/constant';
+import Link from 'next/link';
 import React from 'react'
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <div>
+      {/* overlay */}
+      <div className='fixed inset-0 transform transition-all duration-500 z-[1002] bg-black opacity-70 w-full h-svh'></div>
+      {/* navlinks */}
+      <div className='text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-blue-900 space-y-6 z-[1050]'>
+        {NavLinks.map((link) => {
+          return (
+            <Link key={link.id} href={link.url}>
+              <p className='text-white w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]'>
+                {link.label}
+              </p>
+            </Link>
+          )
+        })}
+        {/* cross icon */}
+      </div>
+    </div>
   )
 }
 
