43c90a6193778ff4c5d791d22cf3258005bf350f
jeremyplembong <jeremyplembong@gmail.com>
2025-11-28 15:05:33 +0800

    finish create account button

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -31,6 +31,21 @@ const Nav = () => {
           })}
         </div>
         {/* buttons */}
+        <div className='flex items-center space-x-4'>
+          {/* 1st button: "Create Account" button */}
+          <a
+            href="#"
+            className='px-5 py-5 relative rounded group font-medium text-white inline-block'
+          >
+            <span className='absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-600'></span>
+            <span className='h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500'></span>
+            <span className='absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500'></span>
+            <span className='absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500'></span>
+            <span className='relative'>Create Account</span>
+          </a>
+          {/* theme toggler */}
+          {/* burger menu */}
+        </div>
       </div>
     </div>
   );
