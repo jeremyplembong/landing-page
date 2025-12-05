520fca6a5d0beadc908d28083df504d595db9879
jeremyplembong <jeremyplembong@gmail.com>
2025-12-05 15:13:29 +0800

    finish testimonials

--- components/Home/Project/Project.tsx
+++ components/Home/Project/Project.tsx
@@ -21,7 +21,7 @@ const Project = () => {
               href="#_"
               className='w-full py-4 text-base sm:text-lg md:text-xl text-center text-white transition-color font-semibold duration-300 bg-blue-800 rounded-full uppercase hover:bg-blue-900 ease px-9 md:w-auto'
             >
-              All Services
+              All Projects
             </a>
           </div>
         </div>
