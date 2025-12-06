320c2535acf8aa4655747cb8a96528ad1186c685
jeremyplembong <jeremyplembong@gmail.com>
2025-12-06 10:42:18 +0800

    finish blog section

---
+++ components/Home/Blog/Blog.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import BlogCard from './BlogCard'
+
+const Blog = () => {
+  return (
+    <div className='pt-16 pb-16'>
+      {/* heading section */}
+      <div className='text-center'>
+        {/* subheading */}
+        <h1 className='uppercase text-lg sm:text-xl md:text-2xl font-bold text-blue-700 dark:text-pink-500'>
+          Latest Blog
+        </h1>
+        {/* heading */}
+        <h1 className='text-xl sm:text-3xl md:text-4xl mt-1 font-bold'>
+          Ready Our Latest Insights from <br /> Our Blog Posts
+        </h1>
+      </div>
+      <div className='w-[80%] mt-16 mx-auto grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6'>
+        <div>
+          <BlogCard 
+            image='/images/b1.png' 
+            title='Expert Advice for Growing Your Digital Presence' 
+          />
+        </div>
+        <div>
+          <BlogCard 
+            image='/images/b2.png' 
+            title='Data-Driven Strategies and Case Studies Success' 
+          />
+        </div>
+        <div>
+          <BlogCard 
+            image='/images/b3.png' 
+            title='The Complete SEO Blog for Digital Succes' 
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Blog
