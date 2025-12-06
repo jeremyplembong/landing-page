a5b120eb10270f0516a5d973eda491249b3ab103
jeremyplembong <jeremyplembong@gmail.com>
2025-12-06 19:35:31 +0800

    finish scroll to top button

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Hero from "./Hero/Hero"
 import Services from './Services/Services'
@@ -8,7 +9,13 @@ import Project from './Project/Project'
 import Review from './Review/Review'
 import Blog from './Blog/Blog'
 
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+
 const Home = () => {
+
+  
+
   return (
     <div className='overflow-hidden'>
       <Hero />
